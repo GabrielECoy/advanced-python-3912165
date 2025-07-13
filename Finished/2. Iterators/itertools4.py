e7c46bc5f8c6834a767fa76705875e8b0de49cba
GabrielECoy <40899132+GabrielECoy@users.noreply.github.com>
2025-07-13 21:59:30 +0000

    Minor itertools tests

--- Finished/2. Iterators/itertools4.py
+++ Finished/2. Iterators/itertools4.py
@@ -11,16 +11,23 @@ deck = list(itertools.product(cards, suits))
 print(len(deck), "cards")
 print(deck)
 
+# Other way
+cards = "A23456789TJQK"
+suits = "SCHD"
+deck = list(itertools.product(suits, cards))
+formatted_deck = [suit + card for suit, card in deck]
+print(formatted_deck)
+
 
 # permutations() creates tuples of a given length with no repeated elements
 teams = ("A","B","C","D")
 result = itertools.permutations(teams, 2)
-print(list(result))
+print("Permutations",list(result))
 
 
 # combinations() will create combinations of a given length with no repeats
 result = itertools.combinations("ABCD", 3)
-print(list(result))
+print("Combinations",list(result))
 
 
 # combinations_with_replacement() will create combinations of a given length with repeats
